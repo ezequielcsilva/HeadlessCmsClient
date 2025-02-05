7dc705ac67ae77b3ed08cbbdbccf6f32d09dfeb5
Ezequiel Cardoso <62252654+ezequielcsilva@users.noreply.github.com>
2025-02-05 19:24:25 -0300

    feat: add new solution and project structure for HeadlessCmsClient

---
+++ src/HeadlessCmsClient.Core/Models/Document.cs
@@ -0,0 +1,10 @@
+﻿namespace HeadlessCmsClient.Core.Models;
+
+public class Document
+{
+    public string Id { get; set; } = string.Empty;
+    public string Title { get; set; } = string.Empty;
+    public int PublishDate { get; set; }
+    public int ExpiryDate { get; set; }
+    public string Content { get; set; } = string.Empty;
+}
