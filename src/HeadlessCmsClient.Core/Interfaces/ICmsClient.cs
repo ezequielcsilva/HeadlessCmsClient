e8e1d8485c1bcf4374473c3ba8fb5131b4cfe6db
Ezequiel Cardoso <62252654+ezequielcsilva@users.noreply.github.com>
2025-02-05 20:51:25 -0300

    feat: update ICmsClient and add CmsClient implementation

--- src/HeadlessCmsClient.Core/Interfaces/ICmsClient.cs
+++ src/HeadlessCmsClient.Core/Interfaces/ICmsClient.cs
@@ -6,7 +6,7 @@ public interface ICmsClient
 {
     Task<DocumentMetadata[]> GetDocumentsMetadataAsync(CancellationToken cancellationToken);
 
-    Task<Document> GetDocumentAsync(string documentId, CancellationToken cancellationToken);
+    Task<Document?> GetDocumentAsync(Guid documentId, CancellationToken cancellationToken);
 
     Task CreateOrUpdateDocumentAsync(Document document, CancellationToken cancellationToken);
 }
---
+++ src/HeadlessCmsClient.Infrastructure/Services/CmsClient.cs
@@ -0,0 +1,35 @@
+﻿using HeadlessCmsClient.Core.Constants;
+using HeadlessCmsClient.Core.Interfaces;
+using HeadlessCmsClient.Core.Models;
+using System.Text;
+using System.Text.Json;
+
+namespace HeadlessCmsClient.Infrastructure.Services;
+
+internal sealed class CmsClient(HttpClient httpClient) : ICmsClient
+{
+    public async Task<DocumentMetadata[]> GetDocumentsMetadataAsync(CancellationToken cancellationToken)
+    {
+        var response = await httpClient.GetAsync(ApiRoutes.GetDocuments, cancellationToken);
+        response.EnsureSuccessStatusCode();
+
+        var data = JsonSerializer.Deserialize<DocumentMetadata[]>(
+            await response.Content.ReadAsStringAsync(cancellationToken));
+
+        return data ?? [];
+    }
+
+    public async Task<Document?> GetDocumentAsync(Guid documentId, CancellationToken cancellationToken)
+    {
+        var response = await httpClient.GetAsync(string.Format(ApiRoutes.GetDocument, documentId), cancellationToken);
+        response.EnsureSuccessStatusCode();
+        return JsonSerializer.Deserialize<Document>(await response.Content.ReadAsStringAsync(cancellationToken));
+    }
+
+    public async Task CreateOrUpdateDocumentAsync(Document document, CancellationToken cancellationToken)
+    {
+        var content = new StringContent(JsonSerializer.Serialize(document), Encoding.UTF8, "application/json");
+        var response = await httpClient.PostAsync(ApiRoutes.CreateOrUpdateDocument, content, cancellationToken);
+        response.EnsureSuccessStatusCode();
+    }
+}
