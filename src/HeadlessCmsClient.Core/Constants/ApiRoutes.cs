cece39abec3ce64a4cb44fd1353ffbcd4b33eb78
Ezequiel Cardoso <62252654+ezequielcsilva@users.noreply.github.com>
2025-02-05 19:34:07 -0300

    feat: add ApiRoutes class for managing API endpoint constants

---
+++ src/HeadlessCmsClient.Core/Constants/ApiRoutes.cs
@@ -0,0 +1,10 @@
+﻿namespace HeadlessCmsClient.Core.Constants;
+
+public static class ApiRoutes
+{
+    public const string Auth = "/auth";
+    public const string RefreshAuth = "/auth/refresh";
+    public const string GetDocuments = "/documents";
+    public const string GetDocument = "/document/{0}";
+    public const string CreateOrUpdateDocument = "/document";
+}
