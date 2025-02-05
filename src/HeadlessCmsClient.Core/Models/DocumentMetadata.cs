f9b33ab55939452ae86b3861f76e6d932caa5e3f
Ezequiel Cardoso <62252654+ezequielcsilva@users.noreply.github.com>
2025-02-05 19:28:56 -0300

    feat: add DocumentMetadata and TokenResponse classes

---
+++ src/HeadlessCmsClient.Core/Models/DocumentMetadata.cs
@@ -0,0 +1,9 @@
+﻿namespace HeadlessCmsClient.Core.Models;
+
+public class DocumentMetadata
+{
+    public string Id { get; set; } = string.Empty;
+    public string Title { get; set; } = string.Empty;
+    public int PublishDate { get; set; }
+    public int ExpiryDate { get; set; }
+}
---
+++ src/HeadlessCmsClient.Core/Models/TokenResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace HeadlessCmsClient.Core.Models;
+
+public class TokenResponse
+{
+    public string BearerToken { get; set; } = string.Empty;
+    public int ExpiryDate { get; set; }
+}
