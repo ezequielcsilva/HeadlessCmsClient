ca60ebda2d16f1e54e3388addb90080939476329
Ezequiel Cardoso <62252654+ezequielcsilva@users.noreply.github.com>
2025-02-05 19:32:24 -0300

    feat: add ICmsClient and ITokenProvider interfaces

---
+++ src/HeadlessCmsClient.Core/Interfaces/ICmsClient.cs
@@ -0,0 +1,12 @@
+﻿using HeadlessCmsClient.Core.Models;
+
+namespace HeadlessCmsClient.Core.Interfaces;
+
+public interface ICmsClient
+{
+    Task<DocumentMetadata[]> GetDocumentsMetadataAsync(CancellationToken cancellationToken);
+
+    Task<Document> GetDocumentAsync(string documentId, CancellationToken cancellationToken);
+
+    Task CreateOrUpdateDocumentAsync(Document document, CancellationToken cancellationToken);
+}
---
+++ src/HeadlessCmsClient.Core/Interfaces/ITokenProvider.cs
@@ -0,0 +1,6 @@
+﻿namespace HeadlessCmsClient.Core.Interfaces;
+
+public interface ITokenProvider
+{
+    Task<string> GetTokenAsync(CancellationToken cancellationToken);
+}
